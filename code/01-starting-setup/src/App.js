bcf360233fcc8c1f38e22d1dbe0b94f073ae9d26
tincurar <tincurares13@gmail.com>
2023-06-26 14:15:47 +0300

    finished section 12-chapters 1-6

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagrahp, setShowParagrahp] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagrahp((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagrahp} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      <h1>
+        React.memo can be used to only update component which have a lot of
+        children to avoid updating them all the time for no reason. BUT this is
+        not always worth it. ONLY when you have a lot of child components.
+      </h1>
     </div>
   );
 }
