9641535b4d36e119af46cad97a98bf9b6814a227
tincurar <tincurares13@gmail.com>
2023-06-29 14:31:39 +0300

    finished section 15

--- code/04-onwards-to-a-more-realistic-example/src/App.js
+++ code/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,27 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./components/hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-custom-hooks-1c3e9-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
+  //     );
 
-      const data = await response.json();
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
 
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const requestConfig = {
+      url: "https://react-custom-hooks-1c3e9-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    };
+
+    const transformTasks = (data) => {
       const loadedTasks = [];
 
       for (const taskKey in data) {
@@ -29,15 +51,10 @@ function App() {
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks(requestConfig, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
