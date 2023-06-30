556304c785901dba8da111207ef0edaf8290ec64
tincurar <tincurares13@gmail.com>
2023-06-30 20:47:20 +0300

    finished section 19

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,13 +1,99 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
+
+let isInitial = true;
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+
+  // ALTERNATIVE TO ADD LOGIC IN COMPONENT
+  // BUT IT SHOULD BE BETTER TO ADD IT INSIDE THE REDUX
+  // WITH CUSTOM ACTION CREATOR THUNK
+
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "pending",
+  //         title: "Sending...",
+  //         message: "Sending cart data.",
+  //       })
+  //     );
+
+  //     const response = await fetch(
+  //       "https://react-http-b8ce9-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+  //       { method: "PUT", body: JSON.stringify(cart) }
+  //     );
+
+  //     if (!response.ok) {
+  //       console.log(response);
+  //       throw new Error("Sending cart data failed.");
+  //     }
+
+  //     // not actually needed here
+  //     // const responseData = await response.json();
+
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "success",
+  //         title: "Success!",
+  //         message: "Sent cart data successfully.",
+  //       })
+  //     );
+  //   };
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "error",
+  //         title: "Error!",
+  //         message: "Sending cart data failed.",
+  //       })
+  //     );
+  //   });
+  // }, [cart, dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
---
+++ code/01-starting-project/src/store/cart-actions.js
@@ -0,0 +1,88 @@
+import { cartActions } from "./cart-slice";
+import { uiActions } from "./ui-slice";
+
+const url =
+  "https://react-http-b8ce9-default-rtdb.europe-west1.firebasedatabase.app/cart.json";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(url); // GET REQUEST IS DEFAULT
+
+      if (!response.ok) {
+        throw new Error("Fetching cart data failed.");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [], // in case items is undefined
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed.",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data.",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully.",
+        })
+      );
+    } catch (error) {
+      sendCartData().catch((error) => {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed.",
+          })
+        );
+      });
+    }
+  };
+};
