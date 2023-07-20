11a641d04c4d06b8e4f211fe340f9247579f190c
tincurar <tincurares13@gmail.com>
2023-07-20 15:24:41 +0300

    finished section 22

--- code/01-starting-project/frontend/src/pages/Root.js
+++ code/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,33 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    const timeoutOneHour = 1 * 60 * 60 * 1000; // time in miliseconds := 1 h
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
